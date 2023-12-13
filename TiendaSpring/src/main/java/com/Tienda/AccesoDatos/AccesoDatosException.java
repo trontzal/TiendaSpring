193ca92a93f5e76ac772ef2c167e669aa2f1edfe
Curso <Curso@Gorbea01.ipartekaula.com>
2023-12-13 12:50:27 +0100

    excepcion

---
+++ TiendaSpring/src/main/java/com/Tienda/AccesoDatos/AccesoDatosException.java
@@ -0,0 +1,8 @@
+package com.Tienda.AccesoDatos;
+
+import lombok.experimental.StandardException;
+
+@StandardException
+public class AccesoDatosException extends RuntimeException {
+	private static final long serialVersionUID = -43565756836L;
+}
