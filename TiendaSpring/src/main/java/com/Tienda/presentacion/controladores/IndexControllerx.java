18bc54269fe0c3ce876793caca9479ec28842f7b
programacion <programacion@DESKTOP-8JKEEE6>
2023-12-15 21:05:52 +0100

    controladores para los html

--- TiendaSpring/src/main/java/com/Tienda/presentacion/controladores/IndexControllerx.java
+++ TiendaSpring/src/main/java/com/Tienda/presentacion/controladores/IndexControllerx.java
@@ -5,13 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.Tienda.logicanegocio.UsuarioNegociox;
 
 @Controller
 @RequestMapping
-public class IndexControlerx {
+public class IndexControllerx {
 	@Autowired
 	private UsuarioNegociox negocio;
 
@@ -25,7 +24,7 @@ public class IndexControlerx {
 
 //	@ResponseBody
 	@GetMapping("/detalle")
-	public String detalle(Model modelo, long id) {
+	public String detalle(Model modelo, Long id) {
 		modelo.addAttribute("producto", negocio.detalleProducto(id));
 		return "detalle";
 	}
