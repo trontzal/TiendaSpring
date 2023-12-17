40836f0bd08d668878f89e72ae790eb1078c5d59
programacion <programacion@DESKTOP-8JKEEE6>
2023-12-17 14:51:34 +0100

    /index a / por defecto

--- TiendaSpring/src/main/java/com/Tienda/presentacion/controladores/IndexControllerx.java
+++ TiendaSpring/src/main/java/com/Tienda/presentacion/controladores/IndexControllerx.java
@@ -15,7 +15,7 @@ public class IndexControllerx {
 	private UsuarioNegociox negocio;
 
 //	@ResponseBody
-	@GetMapping("/index")
+	@GetMapping("/")
 	public String index(Model modelo) {
 //		return negocio.listadoProductos().toString();
 		modelo.addAttribute("productos", negocio.listadoProductos());
