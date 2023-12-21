203dfca2aa29b36843fed87b2d67df65d5545be2
Curso <Curso@Gorbea01.ipartekaula.com>
2023-12-21 13:02:12 +0100

    log y configuracion del error no esperado llamando a la plantilla de error

--- TiendaSpring/src/main/java/com/Tienda/presentacion/controladores/IndexControllerx.java
+++ TiendaSpring/src/main/java/com/Tienda/presentacion/controladores/IndexControllerx.java
@@ -1,6 +1,9 @@
 package com.Tienda.presentacion.controladores;
 
+import java.util.logging.Level;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.Tienda.logicanegocio.UsuarioNegociox;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.java.Log;
+
+@Log
 @Controller
 @RequestMapping
-public class IndexControllerx {
+public class IndexControllerx implements ErrorController{
+	
 	@Autowired
 	private UsuarioNegociox negocio;
 
@@ -27,5 +36,17 @@ public class IndexControllerx {
 	public String detalle(Model modelo, Long id) {
 		modelo.addAttribute("producto", negocio.detalleProducto(id));
 		return "detalle";
+	}
+	
+	@GetMapping("/error")
+	public String error(Model modelo, HttpServletRequest request, Exception exception) {
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		log.severe("Status: " + status.toString());
+		log.severe("Mensaje: " + exception.getMessage());
+		log.log(Level.SEVERE, "Detalles de la excepción", exception);
+
+		modelo.addAttribute("status", status);
+
+		return "_error";
 	}
 }
