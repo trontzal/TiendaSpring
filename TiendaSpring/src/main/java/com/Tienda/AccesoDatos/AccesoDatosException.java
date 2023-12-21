ce3517f60e000ea5f5351c19f633d2d74654a7d3
Curso <Curso@Gorbea01.ipartekaula.com>
2023-12-21 12:45:14 +0100

    retoque excepciones

--- TiendaSpring/src/main/java/com/Tienda/AccesoDatos/AccesoDatosException.java
+++
@@ -1,8 +0,0 @@
-package com.Tienda.AccesoDatos;
-
-import lombok.experimental.StandardException;
-
-@StandardException
-public class AccesoDatosException extends RuntimeException {
-	private static final long serialVersionUID = -43565756836L;
-}
--- TiendaSpring/src/main/java/com/Tienda/AccesoDatos/Daox.java
+++ TiendaSpring/src/main/java/com/Tienda/AccesoDatos/Daox.java
@@ -2,26 +2,26 @@ package com.Tienda.AccesoDatos;
 
 public interface Daox<T> {
 	default Iterable<T> obtenerTodos(){
-		throw new AccesoDatosException("No implementado");
+		throw new UnsupportedOperationException("No implementado");
 	}
 	
 	default T obtenerPorId(Long id) {
-		throw new AccesoDatosException("No implementado");
+		throw new UnsupportedOperationException("No implementado");
 	}
 	
 	default T insertar(T objeto) {
-		throw new AccesoDatosException("No implementado");
+		throw new UnsupportedOperationException("No implementado");
 	}
 	
 	default T modificar(T objeto) {
-		throw new AccesoDatosException("No implementado");
+		throw new UnsupportedOperationException("No implementado");
 	}
 	
 	default void borrar(Long id) {
-		throw new AccesoDatosException("No implementado");
+		throw new UnsupportedOperationException("No implementado");
 	}
 	
 	default long cuantosHay() {
-		throw new AccesoDatosException("No implementado");
+		throw new UnsupportedOperationException("No implementado");
 	}
 }
