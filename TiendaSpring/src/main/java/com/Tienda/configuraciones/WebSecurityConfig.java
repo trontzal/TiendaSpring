d76e1f48a767f3d30c0ddd008955681216da2cce
programacion <programacion@DESKTOP-8JKEEE6>
2023-12-22 21:10:40 +0100

    sql para el rol y nuestros esquemas sin encriptacion

--- TiendaSpring/src/main/java/com/Tienda/configuraciones/WebSecurityConfig.java
+++ TiendaSpring/src/main/java/com/Tienda/configuraciones/WebSecurityConfig.java
@@ -9,8 +9,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.NoOpAuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -21,33 +23,37 @@ public class WebSecurityConfig {
 	// CODIFICACIÓN DE CONTRASEÑAS BCRYPT
 	@Bean
 	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder(12);
+		return NoOpPasswordEncoder.getInstance();
 	}
-	
+
 	// Autenticacion
 	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth, DataSource dataSource)
-		throws Exception{
-		auth.jdbcAuthentication().dataSource(dataSource);
+	public void configureGlobal(AuthenticationManagerBuilder auth, DataSource dataSource) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource)
+			.usersByUsernameQuery(
+					"""
+					SELECT email,password,
+					"""
+					)
+			.authoritiesByUsernameQuery(
+					"""
+					SELECT u.email, CONCAT('ROLE_', r.nombre)
+					FROM usuarios u
+					JOIN roles r ON u.rol_id = r.id
+					WHERE email = ?
+					"""
+					);
 	}
-	
+
 	// AUTORIZACIÓN
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-	        .authorizeHttpRequests(requests -> requests
-                .requestMatchers("/admin/**").hasRole("ADMIN")
-                .requestMatchers("/detalle").authenticated()
-                .anyRequest().permitAll()
+		http.authorizeHttpRequests(requests -> requests.requestMatchers("/admin/**").hasRole("ADMIN")
+				.requestMatchers("/detalle").authenticated().anyRequest().permitAll()
 //				.requestMatchers("/", "/home").permitAll()
 //				.anyRequest().authenticated()
-	        )
-	        .exceptionHandling(handling -> handling.accessDeniedPage("/login?noautorizado"))
-	        .formLogin(form -> form
-                .loginPage("/login")
-                .permitAll()
-	        )
-	        .logout(logout -> logout.permitAll());
+		).exceptionHandling(handling -> handling.accessDeniedPage("/login?noautorizado"))
+				.formLogin(form -> form.loginPage("/login").permitAll()).logout(logout -> logout.permitAll());
 
 		return http.build();
 	}
