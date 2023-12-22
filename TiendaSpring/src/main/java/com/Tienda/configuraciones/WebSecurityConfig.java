3f5e2d2ee72bca49eec7bd71ffef97e134361283
programacion <programacion@DESKTOP-8JKEEE6>
2023-12-22 18:49:11 +0100

    Configuracion del login y las alertas

---
+++ TiendaSpring/src/main/java/com/Tienda/configuraciones/WebSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.Tienda.configuraciones;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import io.micrometer.core.ipc.http.HttpSender.Request;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+	// AUTENTICACIÓN
+	@Bean
+	UserDetailsService userDetailsService() {
+		UserDetails admin = User.withDefaultPasswordEncoder()
+			.username("javier").password("contra").roles("ADMIN")
+			.build();
+		UserDetails user = User.withDefaultPasswordEncoder()
+			.username("pepe").password("perez").roles("USER")
+			.build();
+
+		return new InMemoryUserDetailsManager(user, admin);
+	}
+
+	// AUTORIZACIÓN
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+	        .authorizeHttpRequests(requests -> requests
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/detalle").authenticated()
+                .anyRequest().permitAll()
+//				.requestMatchers("/", "/home").permitAll()
+//				.anyRequest().authenticated()
+	        )
+	        .exceptionHandling(handling -> handling.accessDeniedPage("/login?noautorizado"))
+	        .formLogin(form -> form
+                .loginPage("/login")
+                .permitAll()
+	        )
+	        .logout(logout -> logout.permitAll());
+
+		return http.build();
+	}
+}
--- TiendaSpring/src/main/java/com/Tienda/presentacion/controladores/IndexControllerx.java
+++ TiendaSpring/src/main/java/com/Tienda/presentacion/controladores/IndexControllerx.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.Tienda.bibliotecas.Alerta;
 import com.Tienda.logicanegocio.UsuarioNegociox;
 
 import jakarta.servlet.RequestDispatcher;
@@ -18,8 +19,8 @@ import lombok.extern.java.Log;
 @Log
 @Controller
 @RequestMapping
-public class IndexControllerx implements ErrorController{
-	
+public class IndexControllerx implements ErrorController {
+
 	@Autowired
 	private UsuarioNegociox negocio;
 
@@ -37,13 +38,39 @@ public class IndexControllerx implements ErrorController{
 		modelo.addAttribute("producto", negocio.detalleProducto(id));
 		return "detalle";
 	}
-	
+
+	@GetMapping("/login")
+	public String login(String error, String logout, String noautorizado, String interactivo, Model modelo) {
+		Alerta alerta = new Alerta(modelo);
+
+		if (error != null) {
+			alerta.danger("El usuario o la contraseña no son correctos");
+		} else if (logout != null) {
+			alerta.success("Se ha desconectado la sesion correctamente");
+		} else if (noautorizado != null) {
+			alerta.danger("Tu nivel de acceso  no es suficiente");
+		} else if (interactivo != null) {
+		} else {
+			alerta.warning("Tienes que iniciar sesion");
+		}
+
+		return "login";
+	}
+
 	@GetMapping("/error")
 	public String error(Model modelo, HttpServletRequest request, Exception exception) {
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 		log.severe("Status: " + status.toString());
-		log.severe("Mensaje: " + exception.getMessage());
-		log.log(Level.SEVERE, "Detalles de la excepción", exception);
+//		log.severe("Mensaje: " + exception.getMessage());
+//		log.log(Level.SEVERE, "Detalles de la excepción", exception);
+
+		do {
+			log.severe("Tipo: " + exception.getClass().getSimpleName());
+			log.severe("Mensaje: " + exception.getMessage());
+			log.log(Level.SEVERE, "Detalles de la excepción", exception);
+
+			exception = (Exception) exception.getCause();
+		} while (exception != null);
 
 		modelo.addAttribute("status", status);
 
